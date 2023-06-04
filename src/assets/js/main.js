1b9b78c4934b04cc677e9b952ad80e6b2a4b6fcc
Bruno <github@bbergmann.de>
2023-06-04 12:03:45 +0000

    Solve #7 btn scoll func not working

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,3 +2,7 @@ function scrollToSection() {
     var targetElement = document.getElementById('scrollTarget');
     targetElement.scrollIntoView({ behavior: 'smooth' });
 }
+
+document.getElementById("scrollArrows").onclick =  function (){
+    scrollToSection()
+};
